7b259416238b35a1f0b28d27bd735c67f1b4eb31
Mikhail Cazi <mikhailcazi@gmail.com>
2025-07-15 02:57:44 +0530

    Sprint 8, anagrams

--- src/assets/anagrams.js
+++ src/assets/anagrams.js
@@ -1,7 +1,28 @@
 function isAnagram(string1, string2) {
-  // return true or false
+  // coffee, offcee
+  // charCount = {c: 0, o: 0, f: 0, e: 0}
+
+  if (string1.length !== string2.length) return false;
+
+  let charCount = {};
+
+  for (let char of string1) {
+    // charCount[char] = charCount[char] ? charCount[char] + 1 : 1;
+    charCount[char] = (charCount[char] || 0) + 1;
+  }
+
+  for (let char of string2) {
+    if (!charCount[char]) {
+      return false;
+    }
+    charCount[char]--;
+  }
+
+  return true;
 }
 
 // stressed, desserts
 // tea, ate
 // taco, coat
+
+// land, hair
